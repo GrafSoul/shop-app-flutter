1748e164727ffdc61ffa08aaf3e322fa3f9fe781
GrafSoul <grafsoul@gmail.com>
2022-08-25 19:13:34 +0500

    feat: Added styles for the ProductItem widget

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -11,8 +11,47 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(imageUrl),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+            width: 1.0, color: const Color.fromARGB(255, 208, 208, 208)),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 3.0,
+            offset: Offset(3, 3),
+            spreadRadius: 0.3,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
