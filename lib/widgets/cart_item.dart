27362317c061c19758292371ae018295a22b21aa
GrafSoul <grafsoul@gmail.com>
2022-09-01 17:36:00 +0500

    feat: Updated Cart screen.

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final String title;
   final int quantity;
   final double price;
@@ -12,28 +16,50 @@ class CartItem extends StatelessWidget {
     required this.title,
     required this.quantity,
     required this.price,
+    required this.productId,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        color: Theme.of(context).errorColor,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(
-                child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('\$$price'),
-            )),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('\$$price'),
+              )),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
